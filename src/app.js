391c5d08ce8e918149a381ed76c2c25e8853ad34
Lexxx06 <alexandraokonkwo06@gmail.com>
2024-07-06 16:00:15 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+/* let greeting = "Hello World!";
+console.log(greeting);
+
+prompt("What is your name");
+prompt("What is their name");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your lovescore is " + loveScore + " %");*/
+
+
+
+
+
+
